b2fca0022f0e35e671ff5a8afae50e67ff55b3a6
Pedro Dias <pdr050203@gmail.com>
2023-12-25 16:59:05 -0300

    add: theme colors and a provider for theme

--- App.tsx
+++ App.tsx
@@ -1,11 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
+import { ThemeProvider } from 'styled-components'
+
 import { Groups } from './src/screens/Groups';
+import theme from '@theme/index';
 
 export default function App() {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <Groups />
       <StatusBar style="inverted" />
-    </>
+    </ThemeProvider>
   );
 }
--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,4 +1,3 @@
-import { StatusBar } from 'expo-status-bar';
 import { Container, Title } from './styles';
 
 export function Groups() {
