ef07ed3d508ea1f7e32bf21f32625a9051ddf18c
Pedro Dias <pdr050203@gmail.com>
2023-12-27 18:41:40 -0300

    add: header component

---
+++ src/components/Header/styles.ts
@@ -0,0 +1,15 @@
+import styled from "styled-components/native";
+
+export const Container = styled.View`
+  width: 100%;
+
+  flex-direction: row;
+
+  justify-content: center;
+  align-items: center;
+`
+
+export const Logo = styled.Image`
+  width: 46px;
+  height: 55px;
+`
--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,8 +1,10 @@
+import { Header } from '@components/Header';
 import { Container } from './styles';
 
 export function Groups() {
   return (
     <Container>
+      <Header />
     </Container>
   );
 }
