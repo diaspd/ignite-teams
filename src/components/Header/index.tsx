ac05b199e70fea01e3928c09049a9b7e82dd7dbe
Pedro Dias <pdr050203@gmail.com>
2023-12-27 19:29:31 -0300

    add: icons lib

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,22 @@
-import { Container, Logo } from "./styles";
+import { Container, BackButton, BackIcon, Logo } from "./styles";
 
 import logoImg from '@assets/logo.png';
 
-export function Header() {
+type BackButtonProps = {
+  showBackButton?: boolean;
+}
+
+export function Header({ showBackButton = false }: BackButtonProps) {
   return (
     <Container>
+      { showBackButton &&  
+        (
+          <BackButton>
+           <BackIcon />
+          </BackButton>
+        )
+      }
+
       <Logo source={logoImg} />
     </Container>
   )
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,4 +1,6 @@
-import styled from "styled-components/native";
+import styled, { DefaultTheme } from "styled-components/native";
+
+import { CaretLeft } from 'phosphor-react-native';
 
 export const Container = styled.View`
   width: 100%;
@@ -7,6 +9,16 @@ export const Container = styled.View`
 
   justify-content: center;
   align-items: center;
+`
+
+export const BackButton = styled.TouchableOpacity`
+  flex: 1;
+`
+
+export const BackIcon = styled(CaretLeft).attrs(({theme}: DefaultTheme) => ({
+  size: 36,
+  color: theme.COLORS.WHITE,
+}))`
 `
 
 export const Logo = styled.Image`
