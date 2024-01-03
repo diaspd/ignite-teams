d5088f683f63d4448ea46156e641342b9d4161cc
Pedro Dias <pdr050203@gmail.com>
2024-01-02 23:15:25 -0300

    add: FlatList and state to control component GroupCard

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,3 +1,6 @@
+import { useState } from 'react';
+import { FlatList } from 'react-native';
+
 import { Header } from '@components/Header';
 import { Highlight } from '@components/Highlight';
 import { GroupCard } from '@components/GroupCard';
@@ -5,6 +8,8 @@ import { GroupCard } from '@components/GroupCard';
 import { Container } from './styles';
 
 export function Groups() {
+  const [groups, setGroups] = useState<string[]>(['Grupo da Bagunça', 'Meus Amigos'])
+
   return (
     <Container>
       <Header />
@@ -14,7 +19,16 @@ export function Groups() {
         subtitle="jogue com a sua turma"
       />
 
-      <GroupCard title="Ignite" />
+      <FlatList 
+        data={groups}
+        keyExtractor={item => item}
+        renderItem={({ item }) => (
+          <GroupCard 
+            title={item} 
+          />
+        )}
+      />
+
     </Container>
   );
 }
