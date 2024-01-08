2ab9f9dc854384d7a3a7ead228a0595c0d8ee672
Pedro Dias <pdr050203@gmail.com>
2024-01-08 18:35:52 -0300

    add: navigation through the app

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,6 +1,8 @@
 import { useState } from 'react';
 import { FlatList } from 'react-native';
 
+import { useNavigation } from '@react-navigation/native';
+
 import { Header } from '@components/Header';
 import { Highlight } from '@components/Highlight';
 import { ListEmpty } from '@components/ListEmpty';
@@ -10,7 +12,13 @@ import { Button } from '@components/Button';
 import { Container } from './styles';
 
 export function Groups() {
-  const [groups, setGroups] = useState<string[]>([])
+  const [groups, setGroups] = useState<string[]>([]);
+
+  const navigation = useNavigation();
+
+  function handleNewGroup() {
+    navigation.navigate('new')
+  }
 
   return (
     <Container>
@@ -38,6 +46,7 @@ export function Groups() {
 
       <Button 
         title='Criar nova turma'
+        onPress={handleNewGroup}
       />
 
     </Container>
