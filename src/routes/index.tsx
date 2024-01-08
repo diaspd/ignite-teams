ce6772623fc6b9dcddafa0b0486e1990f7a6394a
Pedro Dias <pdr050203@gmail.com>
2024-01-08 20:08:51 -0300

    fix: loading background when changin screens

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,16 @@
+import { View } from 'react-native';
+
+import { useTheme } from 'styled-components/native';
 import { NavigationContainer } from '@react-navigation/native';
 
 import { AppRoutes } from './app.routes';
 
 export function Routes() {
   return (
-    <NavigationContainer>
-      <AppRoutes />
-    </NavigationContainer>
+    <View style={{ flex: 1, backgroundColor: useTheme().COLORS.GRAY_600}}>
+      <NavigationContainer>
+        <AppRoutes />
+      </NavigationContainer>
+    </View>
   );
 }
