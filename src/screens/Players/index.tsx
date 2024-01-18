e7869d2b19e445013fe55147f74a040556dc6d9b
Pedro Dias <pdr050203@gmail.com>
2024-01-17 22:04:35 -0300

    add: player remove

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -17,6 +17,7 @@ import { Button } from '@components/Button';
 import { PlayerStorageDTO } from '@storage/player/PlayerStorageDTO';
 import { playerAddByGroup } from '@storage/player/playerAddByGroup';
 import { playersGetByGroupAndTeam } from '@storage/player/playerGetByGroupAndTeam';
+import { playerRemoveByGroup } from '@storage/player/playerRemoveByGroup';
 
 import { Container, Form, HeaderList, NumberOfPlayers } from './styles';
 
@@ -68,6 +69,19 @@ export function Players() {
     } catch (error) {
       console.log(error);
       Alert.alert('Pessoas', 'Não foi possível carregar as pessoas do time selecionado.');
+    }
+  }
+
+  async function handlePlayerRemove(playerName: string) {
+    try {
+      await playerRemoveByGroup(playerName, group);
+
+      fetchPlayersByTeam()
+
+    } catch (error) {
+      console.log(error);
+
+      Alert.alert('Remover pessoa', 'Não foi possível remover essa pessoa.');
     }
   }
 
@@ -124,7 +138,7 @@ export function Players() {
         renderItem={({ item }) => (
           <PlayerCard 
             name={item.name} 
-            onRemove={() => {}}
+            onRemove={() => handlePlayerRemove(item.name)}
           />
         )}
         ListEmptyComponent={() => (
